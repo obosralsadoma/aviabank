06e7ae312c0dba878cb0dc4a8fc8e3d849ecb0fa
ойтишный <ccdeter228@gmail.com>
2026-01-03 21:18:54 +0500

    Обновлен конструктор

--- backend/bank.js
+++ backend/bank.js
@@ -8,11 +8,14 @@ class BankAccount {
     constructor(pin) {
         this.number = getRandomInt(10243719, 99999999)
         this.pin = pin
+        this.datecreated = new Date()
+        this.abbrdc = `${this.datecreated.getDate}/${this.datecreated.getMonth + 1}/${this.datecreated.getFullYear}`
+        this.dateendsup = new Date()
+        this.dateendsup.setFullYear += 3
         this.abbrnum = "**" + String(this.number).slice(4, 8)
-        this.blocked
+        this.blocked = false
+        this.bankname = "aviabank"
+        this.balance = 0
+        console.log("Создан банковский аккаунт.")
     }
 }
-
-const bacc = new BankAccount(0)
-console.log(bacc.number)
-console.log(bacc.abbrnum)
